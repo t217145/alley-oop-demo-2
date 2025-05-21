bdd4976a8ede28ddb0774472aa5a6dd0377da864
Cyrus Cheng <cyrus@cyrus822.com>
2025-05-21 15:52:07 +0800

    bug fix

--- src/main/java/com/hkjc/alleyoop/cicd/demo2/DemoController.java
+++ src/main/java/com/hkjc/alleyoop/cicd/demo2/DemoController.java
@@ -1,6 +1,7 @@
 package com.hkjc.alleyoop.cicd.demo2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
